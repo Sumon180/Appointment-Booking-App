11a0ec1decfe7442853318e8963861213fc3eb7e
sumon-hossain <sumonengr.96@gmail.com>
2023-05-30 13:33:35 +0600

    WIP

--- front-end/src/components/DoctorCard.tsx
+++ front-end/src/components/DoctorCard.tsx
@@ -15,7 +15,7 @@ const DoctorCard: React.FC<DoctorCardProps> = ({ doctor, onBookAppointment }) =>
     return (
         <div className="doctor-card bg-blue-500 p-4">
             <h2 className="text-2xl text-white font-bold">{doctor.name}</h2>
-            <p className="text-white">{doctor.specialty}</p>
+            <p className="text-white mb-5">{doctor.specialty}</p>
             <button className="bg-green-500 text-white px-4 py-2 rounded" onClick={() => onBookAppointment(doctor)}>
                 Book Appointment
             </button>
