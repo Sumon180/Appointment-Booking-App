a8868973e5ed39c823179eb2d1540dedc0fb6d47
sumon-hossain <sumonengr.96@gmail.com>
2023-06-01 12:53:29 +0600

    WIP

--- front-end/src/components/AppointmentForm.tsx
+++ front-end/src/components/AppointmentForm.tsx
@@ -67,7 +67,7 @@ const AppointmentForm: FC<AppointmentFormProps> = ({
                     type="submit"
                     className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-5"
                 >
-                    Submit
+                    Book Appointment
                 </button>
                 <button
                     onClick={BackToHome}
