35ca9eb4e443592c617c3f2d2523cb9a7953baf8
sumon-hossain <sumonengr.96@gmail.com>
2023-06-03 23:53:25 +0600

    WIP

--- front-end/src/components/AppointmentList.tsx
+++ front-end/src/components/AppointmentList.tsx
@@ -3,9 +3,18 @@ import { v4 as uuidv4 } from 'uuid';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import { Appointment, AppointmentListProps } from '../types';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const AppointmentList: React.FC<AppointmentListProps> = ({ appointments, onEditAppointment,
     onDeleteAppointment, }) => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulating an asynchronous operation
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    }, []);
 
     const handleEditAppointment = (appointment: Appointment) => {
         onEditAppointment(appointment);
@@ -18,54 +27,57 @@ const AppointmentList: React.FC<AppointmentListProps> = ({ appointments, onEditA
     return (
         <div>
             <h2 className="text-3xl mb-5">Appointment List</h2>
-            {appointments.length > 0 ? (
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Doctor</TableCell>
-                                <TableCell align="right">Patient</TableCell>
-                                <TableCell align="right">Date</TableCell>
-                                <TableCell align="right">Time</TableCell>
-                                <TableCell align="right">Action</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {appointments.map((appointment) => (
-                                <TableRow
-                                    key={uuidv4()}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell component="th" scope="row">
-                                        {appointment.doctor.name}
-                                    </TableCell>
-                                    <TableCell align="right">{appointment.name}</TableCell>
-                                    <TableCell align="right">{appointment.date}</TableCell>
-                                    <TableCell align="right">{appointment.time}</TableCell>
-                                    <TableCell align="right">
-                                        <Link to={`/appointments/${appointment.id}`}>
-                                            <button
-                                                onClick={() => handleEditAppointment(appointment)}
-                                                className="ml-2 bg-blue-500 text-white px-2 py-1 rounded"
-                                            >
-                                                Edit
-                                            </button>
-                                        </Link>
-                                        <button
-                                            onClick={() => handleDeleteAppointment(appointment.id)}
-                                            className="ml-2 bg-rose-500 text-white px-2 py-1 rounded"
-                                        >
-                                            Delete
-                                        </button>
-                                    </TableCell>
+            {isLoading ? (
+                <p className='text-center'>Data Loading...</p>
+            ) :
+                appointments.length > 0 ? (
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Doctor</TableCell>
+                                    <TableCell align="right">Patient</TableCell>
+                                    <TableCell align="right">Date</TableCell>
+                                    <TableCell align="right">Time</TableCell>
+                                    <TableCell align="right">Action</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            ) : (
-                <p className='text-center text-red-500'>No appointments available.</p>
-            )}
+                            </TableHead>
+                            <TableBody>
+                                {appointments.map((appointment) => (
+                                    <TableRow
+                                        key={uuidv4()}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {appointment.doctor.name}
+                                        </TableCell>
+                                        <TableCell align="right">{appointment.name}</TableCell>
+                                        <TableCell align="right">{appointment.date}</TableCell>
+                                        <TableCell align="right">{appointment.time}</TableCell>
+                                        <TableCell align="right">
+                                            <Link to={`/appointments/${appointment.id}`}>
+                                                <button
+                                                    onClick={() => handleEditAppointment(appointment)}
+                                                    className="ml-2 bg-blue-500 text-white px-2 py-1 rounded"
+                                                >
+                                                    Edit
+                                                </button>
+                                            </Link>
+                                            <button
+                                                onClick={() => handleDeleteAppointment(appointment.id)}
+                                                className="ml-2 bg-rose-500 text-white px-2 py-1 rounded"
+                                            >
+                                                Delete
+                                            </button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                ) : (
+                    <p className='text-center text-red-500'>No appointments available.</p>
+                )}
         </div>
     );
 };
