7b8e45e00efe2d606f3249d70ea1ea202e01e409
sumon-hossain <sumonengr.96@gmail.com>
2023-06-05 17:49:14 +0600

    WIP

--- front-end/src/pages/SelectDoctor.tsx
+++ front-end/src/pages/SelectDoctor.tsx
@@ -117,7 +117,7 @@ const SelectDoctor: FC = () => {
                     <div className="doctor-list p-10 bg-white decoration-purple-500 drop-shadow-lg">
                         <h2 className="text-2xl font-semi-bold mb-3 text-slate-700">
                             {editingAppointment ? (
-                                <span className="text-animate text-rose-500">Now, Select a doctor</span>)
+                                <span className="text-animate text-green-600">Now, Select a doctor</span>)
                                 : "Choose a doctor:"}
                         </h2>
                         <div className='flex flex-wrap items-center gap-3'>
